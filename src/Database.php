20fc5ebb5b5fd3514f8591272cdd9c599b39385b
naomai <lulu@nieznani-sprawcy.pl>
2026-08-04 23:22:39 +0200

    feat: implement db insert/upsert

--- src/Database.php
+++ src/Database.php
@@ -1,8 +1,6 @@
 <?php
 namespace Naomai\Compactorium;
 
-use Exception;
-
 class Database {
     private static ?\PDO $db = null;
 
@@ -11,7 +9,7 @@ class Database {
         $dsn = Config::get("DB_DSN");
 
         if($dsn===null) {
-            throw new Exception("Database connection is not configured");
+            throw new \Exception("Database connection is not configured");
         }
 
         self::$db = new \PDO( dsn: $dsn);
@@ -19,7 +17,7 @@ class Database {
 
     public static function connection(): \PDO {
         if (self::$db === null) {
-            throw new Exception("Database is not initialized");
+            throw new \Exception("Database is not initialized");
         }
 
         return self::$db;
@@ -27,5 +25,108 @@ class Database {
 
     public static function timeNow(): string {
         return gmdate('Y-m-d\TH:i:s\Z');
+    }
+
+
+    public static function insert(string $table, array $fields) : int {
+        if(!self::validateFieldName($table)){
+            throw new \InvalidArgumentException("Invalid characters in table name.");
+        }       
+        if(!self::validateFieldList($fields)){
+            throw new \InvalidArgumentException("Invalid characters in field name.");
+        }
+
+
+        $columns = implode(', ', array_keys($fields));
+
+        $placeholders = implode(', ', array_map(
+            fn($k) => ':' . $k,
+            array_keys($fields)
+        ));
+
+        $sql = "INSERT INTO $table 
+            ($columns) 
+            VALUES 
+            ($placeholders)
+        ";
+
+        $db = self::$db;
+        $stm = $db->prepare($sql);
+
+        foreach ($fields as $key => $value) {
+            $stm->bindValue(":$key", $value);
+        }
+
+        $stm->execute();
+
+        return (int)$db->lastInsertId();
+    }
+
+    public static function upsert(string $table, array $fields) : int {
+        if(!self::validateFieldName($table)){
+            throw new \InvalidArgumentException("Invalid characters in table name.");
+        }       
+        if(!self::validateFieldList($fields)){
+            throw new \InvalidArgumentException("Invalid characters in field name.");
+        }
+
+
+        $columns = implode(', ', array_keys($fields));
+
+        $placeholders = implode(', ', array_map(
+            fn($k) => ":{$k}",
+            array_keys($fields)
+        ));
+
+
+        $updates = implode(', ', array_map(
+            fn($k) => "{$k} = :{$k}_dbupsrt",
+            array_keys($fields)
+        ));
+
+        $sql = "INSERT INTO $table 
+            ($columns) 
+            VALUES 
+            ($placeholders)
+            ON CONFLICT DO UPDATE 
+            SET $updates
+        ";
+
+        $db = self::$db;
+        $stm = $db->prepare($sql);
+
+        foreach ($fields as $key => $value) {
+            $stm->bindValue(":{$key}", $value);
+            $stm->bindValue(":{$key}_dbupsrt", $value);
+        }
+
+        $stm->execute();
+
+        return (int)$db->lastInsertId();
+    }
+
+    private static function validateFieldList(array $fields) : bool {
+        foreach($fields as $field=>$value) {
+            if(!self::validateFieldName($field)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static function validateFieldName(string $name) : bool {
+        return preg_match("/^[a-z][a-z0-9_]*$/i", $name)===1;
+    }
+
+
+    public static function createDbTimeFromDateTime(\DateTimeInterface $date) : string {
+        return $date->format(\DateTimeInterface::ISO8601_EXPANDED);
+    }
+
+    public static function createDateTimeFromDbTime(string $isoDate) : \DateTimeImmutable {
+        return \DateTimeImmutable::createFromFormat(
+            \DateTimeInterface::ISO8601_EXPANDED,
+            $isoDate
+        );
     }
 }
