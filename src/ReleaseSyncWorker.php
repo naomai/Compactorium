1d1360943f4ae48abe85c85358d1fb0d7730f807
naomai <lulu@nieznani-sprawcy.pl>
2026-08-20 23:05:24 +0200

    feat: implement disambiguation handling, add Discogs as alternate BCD search

--- src/ReleaseSyncWorker.php
+++ src/ReleaseSyncWorker.php
@@ -7,6 +7,7 @@
     use Naomai\Compactorium\Models\Release;
 use Naomai\Compactorium\Models\Scan;
 use Naomai\Compactorium\Services\CoverArtArchive;
+use Naomai\Compactorium\Services\Discogs;
 
     class ReleaseSyncWorker { 
 
@@ -18,39 +19,36 @@ use Naomai\Compactorium\Services\CoverArtArchive;
 
             foreach($bcds as $bcd) {
                 Logger::debug("ReleaseSyncWorker", "search bcd {$bcd->barcode}");
-                $alb = MusicBrainz::GetAlbumByBarcode($bcd->barcode);
+                //$alb = MusicBrainz::GetAlbumByBarcode($bcd->barcode);
+                $albDiscogs = Discogs::SearchBarcode($bcd->barcode);
+
+                $mastersCount = count($albDiscogs);
+                Logger::debug("ReleaseSyncWorker", "found releases({$mastersCount})");
+
+
+                $lastRelease = null;
                 
-                if($alb!==null) {
-                    $release = $alb->releaseInfo;
 
-                    $albObj = new Album();
-                    $albObj->mbid = $release->{'release-group'}->id;
-                    $albObj->artist = $release->{'artist-credit'}[0]->name;
-                    $albObj->title = $release->title;
-                    $albObj->year = Album::getYearFromMbDate($release->date);
-                    $albObj->musicbrainzData = $release;
-                    $albObj->sync();
+                if($mastersCount == 0) {
+                    $alb = MusicBrainz::GetAlbumByBarcode($bcd->barcode);
 
-                    $relObj = new Release();
-                    $relObj->mbid = $release->id;
-                    $relObj->groupMbid = $albObj->mbid;
-                    $relObj->barcode = $release->barcode;
-                    $relObj->musicbrainzData = $release;
-                    $relObj->sync();
-
+                    $lastRelease = self::saveRelease($alb, null, $bcd->barcode);
+                } else {
+                    foreach($albDiscogs as $master) {
+                        $title = $master->title;
+                        $artist = $master->artists[0]->name;
+                        $alb = MusicBrainz::SearchAlbum("artistname:\"{$artist}\" release:\"{$title}\" barcode:{$bcd->barcode}");
+                        $lastRelease = self::saveRelease($alb, $master, $bcd->barcode);
+                    }
+                }
+                
+                if($lastRelease !== null) {
                     $copyObj = new Copy();
                     $copyObj->libraryId = $bcd->libraryId;
                     $copyObj->ownerId = $bcd->ownerId;
-                    $copyObj->releaseMbid = $relObj->mbid;
+                    $copyObj->releaseMbid = ($mastersCount == 1) ? $lastRelease->mbid : null;
                     $copyObj->scanId = $bcd->id;
                     $copyObj->sync();
-
-
-                    Logger::debug("ReleaseSyncWorker", "got {$albObj->artist} - {$albObj->title}");
-
-                    Logger::debug("ReleaseSyncWorker", "grab album cover");
-                    $coverPath = CoverArtArchive::GetReleaseFrontCover($alb);
-                    Logger::debug("ReleaseSyncWorker", "cover art file: {$coverPath}");
 
                     $tasksDone++;
                 } else{
@@ -60,6 +58,34 @@ use Naomai\Compactorium\Services\CoverArtArchive;
                 $bcd->processed = true;
                 $bcd->sync();
             }
+        }
+
+        private static function saveRelease(object $releaseMb, ?object $masterDiscogs, string $bcd) : Release {
+            $release = $releaseMb->releaseInfo;
+
+            $albObj = new Album();
+            $albObj->mbid = $release->{'release-group'}->id;
+            $albObj->artist = $release->{'artist-credit'}[0]->name;
+            $albObj->title = $release->title;
+            //$albObj->year = Album::getYearFromMbDate($release->date);
+            $albObj->year = $masterDiscogs->year ?? Album::getYearFromMbDate($release->date) ?? 0;
+            $albObj->musicbrainzData = $release;
+            $albObj->sync();
+
+            $relObj = new Release();
+            $relObj->mbid = $release->id;
+            $relObj->groupMbid = $albObj->mbid;
+            $relObj->barcode = $release->barcode ?? $bcd;
+            $relObj->musicbrainzData = $release;
+            $relObj->sync();
+            
+            Logger::debug("ReleaseSyncWorker", "saved {$albObj->artist} - {$albObj->title}");
+
+            Logger::debug("ReleaseSyncWorker", "grab album cover");
+            $coverPath = CoverArtArchive::GetReleaseFrontCover($releaseMb);
+            Logger::debug("ReleaseSyncWorker", "cover art file: {$coverPath}");
+
+            return $relObj;
         }
 
         private static function getPendingBarcodes(\PDO $db) : array {
