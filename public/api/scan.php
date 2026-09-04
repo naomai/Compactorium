4b0fbadee515c78d183bb4ac8543c9a9997c7adb
naomai <lougato@nieznani-sprawcy.pl>
2026-09-04 14:50:45 +0200

    feat(api): add copy resolution andpoint

--- public/api/scan.php
+++ public/api/scan.php
@@ -2,9 +2,11 @@
 
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Naomai\Compactorium\Database;
+use Naomai\Compactorium\Models\Album;
 use Naomai\Compactorium\Models\Library;
 use Naomai\Compactorium\Models\Scan;
 use Naomai\Compactorium\Request;
+use Naomai\Compactorium\Views\LibraryCopyView;
 use Naomai\Compactorium\Views\ScanView;
 
 require __DIR__ . '/../../bootstrap/app.php';
@@ -81,6 +83,49 @@ try {
             ];
             $httpCode = 200;
             break;
+
+        case "PATCH":
+            $query = Request::get();
+            $scanId = $query->int("scan");
+
+            $scan = $em->find(Scan::class, $scanId);
+
+            if($scan->ownerId !== $userId) {
+                throw new Exception("Unauthorized.");
+            }
+            
+
+            $request = Request::content();
+
+
+            if($scan->copy !== null) {
+                if($request->text('albumSlug', "") !== "") {
+                    $copyView = LibraryCopyView::fromCopy($scan->copy);
+                    if(property_exists($copyView, 'disambiguation')) {
+                        $disambig = $copyView->disambiguation->albums;
+
+                        $matchingAlbum = array_find(
+                            $disambig, 
+                            fn($a) => $a->slug==$request->text('albumSlug')
+                        );
+                        if($matchingAlbum===null) {
+                            throw new Exception("Invalid album selection for barcode");
+                        }
+                        $scan->copy->album = $em->find(Album::class, $matchingAlbum->slug);
+                        $em->persist($scan->copy);
+                    }
+
+                }
+            }
+            
+            $em->persist($scan);
+            $em->flush();
+
+            $httpCode = 200;
+            $response = ScanView::fromScan($scan);
+
+            break;
+
         default:
             throw new Exception("Unsupported request method.");
     }
